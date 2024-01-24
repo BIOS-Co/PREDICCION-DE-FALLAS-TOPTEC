09ba3b22b6dae690197689bcd7aaebbc38c080cf
jonresident <jonresident@hotmail.com>
2024-01-24 12:15:22 -0500

    Ajuste menor

--- src/app/shared/splashScreen/SplashScreen.jsx
+++ src/app/shared/splashScreen/SplashScreen.jsx
@@ -20,7 +20,7 @@ export default function SplashScreen() {
       <div className={`splash-screen ${showAnimation ? 'animate__animated slideUp' : 'animate__animated animate__fadeIn'}`}>
         <div className='row'>
           <div className='col-12 d-flex flex-column justify-content-center align-items-center align-self-center'>
-            <img className='logo-toptec-splashscreen- mb-3' src={LogoToptec} alt="logo-toptec" />
+            <img className='logo-toptec-splashscreen- mb-2' src={LogoToptec} alt="logo-toptec" />
             <h3 className="m-0 p-0 lh-sm fs-2- font-oswald-regular- text-uppercase text-center fw-bold tx-primary-blue- le-spacing-1-">Predicción de fallas</h3>
           </div>
         </div>
