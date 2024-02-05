b87e702f341b907b042a154505f8edb98e2c40eb
jonresident <jonresident@hotmail.com>
2024-02-05 15:59:29 -0500

    Ajustes menores

--- src/app/landing/Landing.jsx
+++ src/app/landing/Landing.jsx
@@ -73,7 +73,7 @@ export default function Landing() {
             <h3 className="m-0 p-0 lh-sm fs-1- font-oswald-regular- text-uppercase text-center fw-bold tx-primary-blue- le-spacing-1-">Predicción de fallas</h3>
           </div>
         </div>
-        <div className='row mt-4 mb-4 wrapper-buttons-landing-'>
+        <div className='row m-0 mt-4 mb-4 p-0 wrapper-buttons-landing-'>
           <div className='col-12'>
             <div className="row gx-2 d-flex flex-row justify-content-center align-items-center align-self-center">
               <div className="col-auto p-3">
